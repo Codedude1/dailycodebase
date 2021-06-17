4bf94e2298416c31f78314143d95846c0183fad8
Yash Mishra <53789050+Codedude1@users.noreply.github.com>
2021-06-17 13:03:59 +0530

    Add files via upload

--- Day1/C++/day1.cpp
+++ Day1/C++/day1.cpp
@@ -1,26 +1,30 @@
-/**
- * @author: dhruv-gupta14
- * @date: 21-12-2018
- */
-
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
+int main(){
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
 
-int main() {
-	int n;
-	cin >> n;
-	for(int i = 1; i <= n; i++){
-	    if( i%3 == 0 && i%5 == 0){
-	        cout << "FizzBuzz, ";
-	    }
-	    else if( i % 3 == 0){
-	        cout << "Fizz, ";
-	    }
-	    else if( i % 5 == 0){
-	        cout << "Buzz, ";
-	    }
-	    else
-	    cout << i << ", ";
-	}
-	return 0;
+
+int n;
+cin>>n;
+for(int i = 1; i<=n; i++){
+    if((i%3 == 0) & (i%5 == 0)){
+        cout<<"FizzBuzz"<<endl;
+    }
+    else if(i%3 == 0){
+        cout<<"Fizz"<<endl;
+    }
+    else if(i%5 == 0){
+        cout<<"Buzz"<<endl;
+    }
+    
+    else{
+        cout<<i<<endl;
+    }
+}
+return 0;
 }
